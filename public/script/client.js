0602b6a6a3738eb957786e729e2bd21610082981
Glitch (fcc-stock-trace) <none>
2018-01-07 05:36:55 +0000

    🐦🔔 Checkpoint public/script/client.js:596315/22

--- public/script/client.js
+++ public/script/client.js
@@ -26,3 +26,31 @@ $(function(){
 
 
 });
+
+ /*   <div  class="fragment" >
+        <span id='close'>x</span>
+asdasdasd
+</div>
+
+.fragment {
+    font-size: 12px;
+    font-family: tahoma;
+    width: 100px;
+    border: 1px solid #ccc;
+    color: #555;
+    display: block;
+    padding: 10px;
+    box-sizing: border-box;
+    text-decoration: none;
+}
+
+.fragment:hover {
+    box-shadow: 2px 2px 5px rgba(0,0,0,.2);
+
+}
+#close {
+    float:right;
+    display:inline-block;
+    padding:2px 5px;
+    background:#ccc;
+}*/
