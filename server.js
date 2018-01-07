69eb9ab94d6ec080526d5d59b5401df4d42d672f
Glitch (fcc-stock-trace) <none>
2018-01-07 04:36:01 +0000

    ⏰🙉 Checkpoint README.md:596315/36 server.js:596315/71 package.json:596315/17

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
-
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require("./routes/routes");
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
 
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/",express.static('public'));
+
+app.use(session({
+    secret: 'turfanda',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use("/", routes);
 
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.render("error",{error: err.status+" "+err.message});
 });
 
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
