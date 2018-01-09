f1448a6c99897fe7f24b7fec5e4adf9b471b290f
Glitch (fcc-stock-trace) <none>
2018-01-09 08:49:06 +0000

    🐘👧 Checkpoint public/script/client.js:596315/124

--- public/script/client.js
+++ public/script/client.js
@@ -1,4 +1,4 @@
-var stocks=[];
+var stocks = [];
 var socket;
 
 function makeid() {
@@ -12,55 +12,52 @@ function makeid() {
 }
 
 
-$(function(){
-  socket = io();
-  socket.on(function(data){
-  console.log(data);
-  });
-  socket.on('stockArray',function(data){
-    $(".stockZone").remove();
-    stocks=data;
-    var x =$("<div>").addClass("stockZone");
-    $.each(data,function(index,item){
-    x.append($("<div>").attr("id",item).text(item).addClass("stockBox").append($("<span class='closeBtn'>x</span>")));   
+$(function() {
+    socket = io();
+    socket.on(function(data) {
+        console.log(data);
     });
-    $(".container").append(x);
-    
-  });
-  $(".addBtn").on("click",function(){
-    if(stocks.length>9){
-      alert("you can view up to 10 stock");
-      return;
-    }
-    else{
-      var stockName=makeid();
-      stocks.push(stockName);
-      socket.emit("stockArray",stocks);
-      console.log(stocks);
-      $(".stockZone").append($("<div>").attr("id",stockName).text(stockName).addClass("stockBox").append($("<span class='closeBtn'>x</span>")));
-    } 
-  });
-   $(document).on('click',".closeBtn",function(){
-     stocks = $.grep(stocks, function(value) {
-       return value != $(this).parent().attr("id");
-     });
-    $(this).parent().remove();
-           console.log(stocks);
-           socket.emit("stockArray",stocks);
-  });
-  $(".stockBtn").on("click",function(){
-    $.ajax({
-      url: "https://www.alphavantage.co/query?function="+"TIME_SERIES_MONTHLY_ADJUSTED"+"&symbol="+"MSFxxT"+"&apikey="+"HEUIOKJSMVGQQKFR",
-      type:"get",
-      success: function(data){
-        if(data["Error Message"])
-        {
-          alert("Invalid Stock Code");
+    socket.on('stockArray', function(data) {
+        $(".stockZone").remove();
+        stocks = data;
+        var x = $("<div>").addClass("stockZone");
+        $.each(data, function(index, item) {
+            x.append($("<div>").attr("id", item).text(item).addClass("stockBox").append($("<span class='closeBtn'>x</span>")));
+        });
+        $(".container").append(x);
+
+    });
+    $(".addBtn").on("click", function() {
+        if (stocks.length > 9) {
+            alert("you can view up to 10 stock");
+            return;
+        } else {
+            var stockName = makeid();
+            stocks.push(stockName);
+            socket.emit("stockArray", stocks);
+            console.log(stocks);
+            $(".stockZone").append($("<div>").attr("id", stockName).text(stockName).addClass("stockBox").append($("<span class='closeBtn'>x</span>")));
         }
-    console.log(data);
-  }
-});
-  });
+    });
+    $(document).on('click', ".closeBtn", function() {
+      stocks=[];
+
+        $(this).parent().remove();
+        console.log(stocks);
+        socket.emit("stockArray", stocks);
+    });
+    $(".stockBtn").on("click", function() {
+        $.ajax({
+            url: "https://www.alphavantage.co/query?function=" + "TIME_SERIES_MONTHLY_ADJUSTED" + "&symbol=" + "MSFxxT" + "&apikey=" + "HEUIOKJSMVGQQKFR",
+            type: "get",
+            success: function(data) {
+                if (data["Error Message"]) {
+                    alert("Invalid Stock Code");
+                }
+                console.log(data);
+            }
+        });
+    });
 
 
 });
