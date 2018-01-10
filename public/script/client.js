f72215c45e511af5e0931ed3337ffcbc6d866616
Glitch (fcc-stock-trace) <none>
2018-01-10 05:30:08 +0000

    😿⏳ Checkpoint views/index.html:596315/112 public/script/client.js:596315/118

--- public/script/client.js
+++ public/script/client.js
@@ -36,6 +36,65 @@ function getfinanceinfo() {
 
 }
 
+
+
+var seriesOptions = [],
+    seriesCounter = 0;
+
+function createChart() {
+
+    Highcharts.stockChart('chartZone', {
+
+        rangeSelector: {
+            selected: 4
+        },
+
+        yAxis: {
+            labels: {
+                formatter: function () {
+                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                }
+            },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+            }]
+        },
+
+        plotOptions: {
+            series: {
+                compare: 'percent',
+                showInNavigator: true
+            }
+        },
+
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2,
+            split: true
+        },
+
+        series: seriesOptions
+    });
+
+}
+$.each(stockVal, function (i, name) {
+
+      seriesOptions[i] = {
+            name: name,
+            data: data
+        };
+      seriesCounter += 1;
+
+        if (seriesCounter === names.length)
+            createChart();
+        
+    });
+
+
+  
+
 $(function() {
     if (!apikey)
         getapikey();
